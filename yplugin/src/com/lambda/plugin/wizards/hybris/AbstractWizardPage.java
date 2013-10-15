37361949a7ace657013b79def7b632db4261899b
wojtuch <wojciech.poltorak@gmail.com>
2013-10-15 23:51:05 +0200

    import platform wizard

---
+++ yplugin/src/com/lambda/plugin/wizards/hybris/AbstractWizardPage.java
@@ -0,0 +1,26 @@
+package com.lambda.plugin.wizards.hybris;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.layout.GridLayout;
+
+public abstract class AbstractWizardPage extends WizardPage {
+
+    protected AbstractWizardPage(String pageName) {
+        super(pageName);
+    }
+
+    protected GridLayout initGridLayout(GridLayout layout, boolean margins) {
+        layout.horizontalSpacing = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_SPACING);
+        layout.verticalSpacing = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_SPACING);
+        if (margins) {
+            layout.marginWidth = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_MARGIN);
+            layout.marginHeight = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_MARGIN);
+        } else {
+            layout.marginWidth = 0;
+            layout.marginHeight = 0;
+        }
+        return layout;
+    }
+
+}
